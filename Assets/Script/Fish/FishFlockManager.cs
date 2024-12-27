8846ffb472caf0d16325b4cd6fc9d363c8cb83d3
zhangqingfan <100165289+zhangqingfan@users.noreply.github.com>
2024-12-27 17:21:01 -0500

    2

--- Assets/Script/Fish/FishFlockManager.cs
+++ Assets/Script/Fish/FishFlockManager.cs
@@ -71,7 +71,7 @@ public class FishFlockManager : MonoBehaviour
                 fishFlockTargets[i] = pos + gameObject.transform.position;
             }
         }
-    }
+    } 
 
     IEnumerator FlockMove()
     {
