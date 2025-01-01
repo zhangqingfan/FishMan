449c360f85bc4155b78b1f0a10c43882155dd1ea
zhangqingfan <100165289+zhangqingfan@users.noreply.github.com>
2025-01-01 17:45:13 -0500

    ship

--- Assets/ArtResources/Sail/Swing.hlsl
+++ Assets/ArtResources/Sail/Swing.hlsl
@@ -1,7 +1,17 @@
-void Swing_float(float3 position, out float3 result)
+void Swing_float(float3 position, float strength, out float3 result)
 {
-	half t = _Time.x * 1;
-    position += sin(t) * 1;
+    float offset = position.x + position.y + position.z;
+    offset = offset / 3;
+
+    half t = _Time.x * _SwingSpeed;
+    position.z += sin(t + offset * 5)  * strength * 0.2;
 	result = position;
 }
 
+void Strength_float(float2 uv, out float strength)
+{
+    float2 dist = uv - float2(0.5, 0.5);
+    float length = sqrt(dist.x * dist.x + dist.y * dist.y);
+    length = length / 0.7;
+    strength = saturate(1 - length);
+}
